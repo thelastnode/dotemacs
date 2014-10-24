0ab3d50a4b1310c6573340c3aad6b81fec28c3a1
Ankit Shankar <ashankar@palantir.com>
2014-10-24 01:31:49 -0700

    Fill column only in Python

--- el-get-user/init-fill-column-indicator.el
+++ el-get-user/init-fill-column-indicator.el
@@ -1,6 +1,2 @@
-;; enable fill column indicator everywhere
-(defun c/enable-fci-mode ()
-  (set-fill-column 80)
-  (fci-mode 1))
-(define-globalized-minor-mode global-fci-mode fci-mode c/enable-fci-mode)
-(global-fci-mode 1)
+;; enable column 80 indicator for certain buffer types
+(add-hook 'python-mode-hook 'fci-mode)
