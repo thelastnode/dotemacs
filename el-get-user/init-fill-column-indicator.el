e100c0d06034534d743a21fabfb5e3d36b44a830
Ankit Shankar <ashankar@palantir.com>
2014-10-24 01:41:12 -0700

    Fix fill column highlight

--- el-get-user/init-fill-column-indicator.el
+++ el-get-user/init-fill-column-indicator.el
@@ -1,2 +1,5 @@
 ;; enable column 80 indicator for certain buffer types
-(add-hook 'python-mode-hook 'fci-mode)
+(defun c/highlight-column-80 ()
+  (set-fill-column 79)
+  (fci-mode 1))
+(add-hook 'python-mode-hook 'c/highlight-column-80)
